b27bdb0cbd1698b6d8a29bad331920b45a6382fa
Eric Moore <74945568+HumbleResilience@users.noreply.github.com>
2021-11-12 08:30:00 -0600

    Add files via upload

---
+++ functions/script.js
@@ -0,0 +1,47 @@
+// //Define the function
+// //Call the function
+
+// function sayHello(userName ='User'){
+//     console.log("hello " + userName + "!");
+// }
+
+// function max(number1, number2){
+//     if(number1>number2){
+//         console.log(number1);
+
+//     }else{
+//         console.log(number2);
+//     }
+// }
+
+// max(5,9);
+// max(100, 12);
+// max(15, 20)
+
+let x = Number(prompt("Enter number 1: "));
+let y = Number(prompt("Enter number 2: "));
+
+function sum(number1, number2){
+    document.write(`<p>${number1} + ${number2} = ${number1 + number2}</p>`);
+}
+
+function sub(number1, number2){
+    document.write(`<p>${number1} - ${number2} = ${number1 - number2}</p>`);
+}
+
+function mult(number1, number2){
+    document.write(`<p>${number1} * ${number2} = ${number1 * number2}</p>`);
+}
+
+function div(number1, number2){
+    document.write(`<p>${number1} / ${number2} = ${number1/ number2}</p>`);
+}
+
+sum(x, y);
+sub(x,y);
+mult(x,y);
+div(x,y);
+
+
+
+
